083624c28216d6ccaad92962b282e37c332caf8a
Rahul Das <rahulrd077@gmail.com>
2019-01-27 11:02:13 +0530

    Hi this is Rahul , am commiting

--- src/main/java/projpackager/TestDemo/LocatorTest.java
+++ src/main/java/projpackager/TestDemo/LocatorTest.java
@@ -1,5 +1,8 @@
 package projpackager.TestDemo;
 
+import java.util.Set;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -7,15 +10,23 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LocatorTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		WebDriverManager.firefoxdriver().setup();
+		System.setProperty("webdriver.gecko.driver", "D:\\n\\Gecko\\geckodriver.exe");
+		
+		
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("https://www.facebook.com");
 		
+		Set<String> mainwindow = driver.getWindowHandles();
 		
-
+		System.out.println(mainwindow);
+		
+		Object[] window = mainwindow.toArray();
+		
+		System.out.println(window[1]);
+		
 	}
 
 }
